47bca30d9f97382f4323779cb04dbc37914769df
Kirill <blame.le77@gmail.com>
2025-03-14 01:00:18 +0200

    xD

--- js/index.js
+++ js/index.js
@@ -3,3 +3,24 @@ fetch("https://catfact.ninja/fact")
   .then((data) => {
     document.getElementById("catFacts").innerHTML = data.fact;
   });
+
+const carouselInner = document.querySelector(".carousel-inner");
+const prevButton = document.querySelector(".prev-btn");
+const nextButton = document.querySelector(".next-btn");
+const items = document.querySelectorAll(".carousel-item");
+let currentIndex = 0;
+
+function updateCarousel() {
+  const offset = -currentIndex * 100;
+  carouselInner.style.transform = `translateX(${offset}%)`;
+}
+
+nextButton.addEventListener("click", () => {
+  currentIndex = (currentIndex + 1) % items.length;
+  updateCarousel();
+});
+
+prevButton.addEventListener("click", () => {
+  currentIndex = (currentIndex - 1 + items.length) % items.length;
+  updateCarousel();
+});
